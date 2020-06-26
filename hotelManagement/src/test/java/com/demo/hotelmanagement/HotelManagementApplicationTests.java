a1fc899afd474abdb7a8909f5e71134c6fbc59e4
grandhe10 <sumagrandhe10@gmail.com>
2020-06-26 20:32:50 +0530

    changes in pom

--- hotelManagement/src/test/java/com/demo/hotelmanagement/HotelManagementApplicationTests.java
+++ hotelManagement/src/test/java/com/demo/hotelmanagement/HotelManagementApplicationTests.java
@@ -1,13 +1,31 @@
 package com.demo.hotelmanagement;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.demo.hotelmanagement.controller.BookingController;
+import com.demo.hotelmanagement.controller.HotelController;
 
 @SpringBootTest
 class HotelManagementApplicationTests {
 
+	@Autowired
+	HotelController hotelController;
+	@Autowired
+	BookingController bookingController;
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(hotelController).isNotNull();
 	}
 
+	@Test
+	public void contexLoads1() throws Exception {
+		assertThat(bookingController).isNotNull();
+	}
 }
+
+
+	
